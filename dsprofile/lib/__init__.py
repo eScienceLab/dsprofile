d2e35fbbd6805b7f9aeaafc468eef2369dacfc0f
Gary Lloyd <gary.lloyd@manchester.ac.uk>
2026-06-25 11:13:28 +0100

    Export CSV reader

--- dsprofile/lib/__init__.py
+++ dsprofile/lib/__init__.py
@@ -8,3 +8,4 @@ from .netcdf import NetCDFReader           # noqa: F401
 from .tiff import GeoTIFFReader            # noqa: F401
 from .shape import ShapefileReader         # noqa: F401
 from .geopackage import GeoPackageReader   # noqa: F401
+from .csv import CSVReader                 # noqa: F401
